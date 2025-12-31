7feef43fc05657711f4079e6a98df862bfb5a774
wjdwngus2056-jpg <wjdwngus2056@gmail.com>
2025-12-31 09:02:38 +0900

    [level 0] Title: 각도기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/0/120829. 각도기/각도기.cpp
+++ 프로그래머스/0/120829. 각도기/각도기.cpp
@@ -3,23 +3,29 @@
 
 using namespace std;
 
-int solution(int angle) {
-    int answer = 0;
-    
-    if (angle > 0 && angle < 90)
+int JudgeAngle(int angle_) 
+{
+    if (angle_ > 0 && angle_ < 90)
     {
         return 1;
     }
-    else if(angle == 90)
+    else if(angle_ == 90)
     {
         return 2;
     }
-    else if(angle > 90 && angle < 180)
+    else if(angle_ > 90 && angle_ < 180)
     {
         return 3;
     }
-    else if(angle == 180)
+    else if(angle_ == 180)
     {
         return 4;
     }
+}
+
+int solution(int angle) {
+    int answer = 0;
+    answer = JudgeAngle(angle);
+    
+    return answer;
 }
