160aafbab8985bc6b1cc79805292384f4ac225c5
wjdwngus2056-jpg <wjdwngus2056@gmail.com>
2026-01-29 09:12:45 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 1.42 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.cpp
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.cpp
@@ -1,26 +1,17 @@
 #include <string>
 #include <vector>
-#include <deque>
 
 using namespace std;
 
 string solution(vector<int> food) {
     string answer = "";
-    deque<int> food_arr;
-    
-    food_arr.push_back(0);
-    
+    answer += '0';
     for (int i = food.size()-1; i > 0; i--)
     {
         for (int j = 0; j < food[i]/2; j++)
         {
-            food_arr.push_front(i);
-            food_arr.push_back(i);
+            answer = to_string(i) + answer + to_string(i);
         }
-    }
-    for (const int& dish : food_arr)
-    {
-        answer += to_string(dish);
     }
     
     return answer;
