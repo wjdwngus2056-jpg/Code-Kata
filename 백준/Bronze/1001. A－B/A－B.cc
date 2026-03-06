087950312481905134c9b1881bcaec90be05d459
wjdwngus2056-jpg <wjdwngus2056@gmail.com>
2026-03-06 16:48:15 +0900

    [Bronze V] Title: A-B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int A;
+    int B;
+    
+    cin >> A;
+    cin >> B;
+    
+    cout << A-B;
+    
+    return 0;
+}
