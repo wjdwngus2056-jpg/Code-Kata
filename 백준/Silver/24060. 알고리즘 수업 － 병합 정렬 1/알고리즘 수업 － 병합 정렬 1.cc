d4eef116ca9026c38dca6ec3a2055cad49c39570
wjdwngus2056-jpg <wjdwngus2056@gmail.com>
2026-03-10 13:05:03 +0900

    [Silver III] Title: 알고리즘 수업 - 병합 정렬 1, Time: 116 ms, Memory: 5932 KB -BaekjoonHub

--- 백준/Silver/24060. 알고리즘 수업 － 병합 정렬 1/알고리즘 수업 － 병합 정렬 1.cc
+++ 백준/Silver/24060. 알고리즘 수업 － 병합 정렬 1/알고리즘 수업 － 병합 정렬 1.cc
@@ -1,59 +1,60 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 int N, K;
-int A[500001] = { 0 };
-int sorted_arr[500001] = { 0 };
 int counter = 0;
+int answer = -1;
 
-void merge(int* A, int p, int q, int r)
+void merge_arr(vector<int>& arr, vector<int>& tmp, int p, int q, int r)
 {
     int i = p;
     int j = q + 1;
-    int t = 0;
+    int t = p;
+
     while (i <= q && j <= r)
     {
-        if (A[i] <= A[j])
+        if (arr[i] <= arr[j])
         {
-            sorted_arr[t++] = A[i++];
+            tmp[t++] = arr[i++];
         }
         else
         {
-            sorted_arr[t++] = A[j++];
+            tmp[t++] = arr[j++];
         }
     }
+
     while (i <= q)
     {
-        sorted_arr[t++] = A[i++];
-    }
-    while (j <= r)
-    {
-        sorted_arr[t++] = A[j++];
+        tmp[t++] = arr[i++];
     }
 
-    i = p;
-    t = 0;
-    while (i <= r)
+    while (j <= r)
     {
-        A[i++] = sorted_arr[t++];
+        tmp[t++] = arr[j++];
+    }
+
+    for (int idx = p; idx <= r; idx++)
+    {
+        arr[idx] = tmp[idx];
         counter++;
+
         if (counter == K)
         {
-            cout << A[i - 1];
+            answer = arr[idx];
         }
     }
 }
 
-void merge_sort(int* A, int p, int r)
+void merge_sort(vector<int>& arr, vector<int>& tmp, int p, int r)
 {
-    if (p < r)
-    {
-        int q = (p + r) / 2;
-        merge_sort(A, p, q);
-        merge_sort(A, q + 1, r);
-        merge(A, p, q, r);
-    }
+    if (p >= r) return;
+
+    int q = (p + r) / 2;
+    merge_sort(arr, tmp, p, q);
+    merge_sort(arr, tmp, q + 1, r);
+    merge_arr(arr, tmp, p, q, r);
 }
 
 int main()
@@ -63,16 +64,17 @@ int main()
     cout.tie(NULL);
 
     cin >> N >> K;
+
+    vector<int> arr(N);
+    vector<int> sorted_arr(N);
+
     for (int i = 0; i < N; i++)
     {
-        cin >> A[i];
+        cin >> arr[i];
     }
 
-    merge_sort(A, 0, N - 1);
-    if (counter < K)
-    {
-        cout << -1;
-    }
+    merge_sort(arr, sorted_arr, 0, N - 1);
 
+    cout << answer;
     return 0;
 }
